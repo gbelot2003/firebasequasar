62391a3bbe894593f46d96c35546a7ba1dbbbfd9
Gerardo Belot <gbelot2003@hotmail.com>
2019-06-18 23:47:50 -0600

    configurando recivo de informacion desde VerRegistro

--- src/pages/VerRegistro.vue
+++ src/pages/VerRegistro.vue
@@ -1,10 +1,39 @@
 <template>
-    <h5>Ver Registro</h5>
+    <h5>{{ name }}</h5>
 </template>
 
 <script>
+    import db from '../conf/firebaseInit';
+
     export default {
         name: 'VerRegistro',
+        data() {
+          return {
+            employee_id: null,
+            name: null,
+            dept: null,
+            position: null,
+          };
+        },
+        created() {
+          console.log(this.$route.params);
+          this.ferchData();
+        },
+        methods: {
+          ferchData() {
+            db.collection('employees')
+              .where('employee_id', '==', this.$route.params.id).get()
+              .then((rest) => {
+                rest.forEach((doc) => {
+                  console.log(doc.data());
+                  this.employee_id = doc.data().employee_id;
+                  this.name = doc.data().name;
+                  this.dept = doc.data().dept;
+                  this.position = doc.data().position;
+                });
+              });
+          },
+        },
     };
 </script>
 
